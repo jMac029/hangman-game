7664f0b7c16e28bd055e007d3bee018ee0a2172e
James McMillan <jmcmillan29@gmail.com>
2017-10-18 14:23:28 -0700

    fixed r2-d2 name in the array of phrases

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -43,7 +43,7 @@ window.onload = () => {
 						"PRINCESS-LEIA",
 						"OBI-WON KENOBI",
 						"JAR-JAR BINKS",
-						"RD-D2",
+						"R2-D2",
 						"C-3PO",
 						"BB-8",
 						"POE-DAMARON",
